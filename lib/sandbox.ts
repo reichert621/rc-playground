0608136f4c928a1d43729e383d9c1c6526253d22
Alex Reichert <reichertjalex@gmail.com>
2024-03-06 15:37:50 -0500

    This commit was AI generated

--- lib/sandbox.ts
+++ lib/sandbox.ts
@@ -1,1 +1,18 @@
-// TODO: implement!
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+function divide(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+}
