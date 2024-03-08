0b6f1edc25d9f43f1e945f338a32fcb8041bdbf1
Alex Reichert <reichertjalex@gmail.com>
2024-03-08 14:30:42 -0500

    This commit was AI generated

--- lib/sandbox.ts
+++ lib/sandbox.ts
@@ -44,3 +44,29 @@ class Calculator {
     }
 
 }
+function bubbleSort(numbers: number[]): number[] {
+    let len = numbers.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    return numbers;
+}
+
+function generateRandomNumbers(): number[] {
+    let numbers: number[] = [];
+    for (let i = 0; i < 6; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+    }
+    return numbers;
+}
+
+function sortRandomNumbers(): number[] {
+    let numbers = generateRandomNumbers();
+    return bubbleSort(numbers);
+}
